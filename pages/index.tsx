3ad454b7ebbe88806c755c38296adaf2d82983f1
pinegirl2000 <pinegirl2000@gmail.com>
2026-05-19 19:28:17 +0800

    hero 소구문 카피 수정: '셀 친구과 함께 매일매일 영적 습관을 쌓아보세요'

--- pages/index.tsx
+++ pages/index.tsx
@@ -258,7 +258,7 @@ const Home = ({ profileId, displayName, nickname, email, systemAdminHref, menusE
               margin: '0 auto 2.25rem',
               letterSpacing: '0.01em',
             }}>
-              셀 친구들을 초대해, 매일매일 영적 습관을 쌓아보세요.
+              셀 친구과 함께 매일매일 영적 습관을 쌓아보세요.
             </p>
 
             {/* CTA 버튼 */}
